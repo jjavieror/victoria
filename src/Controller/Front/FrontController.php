09be985c25e451da2e6474f81f95f7abc5f59713
Jonathan Olarte <jonathan.olarte@nivelics.com>
2019-10-09 18:07:45 -0500

    a

--- src/Controller/Front/FrontController.php
+++ src/Controller/Front/FrontController.php
@@ -10,7 +10,17 @@ class FrontController extends AbstractController
 
     public function index(Request $request)
     {
-        return $this->render('base.html.twig');
+        $meta = [
+            'title' => 'Xibalba te lleva al inframundo con Victoria | Cerveza Victoria',
+            'description' => 'Ingresa ahora y descubre cómo viajar al inframundo de Xibalba con Cerveza Victoria. Una experiencia que jamás olvidarás. Bienvenido, ¡descubre más aquí!',
+        ];
+        if($request->getRequestUri() === '/register') {
+            $meta['title'] = 'Xibalba chingones hasta en la muerte | Cerveza Victoria';
+            $meta['description'] = 'Xibalba quiere saber si eres chingón hasta en la muerte. Regístrate en nuestro sitio de Cerveza Victoria y participa por un viaje a la Rivera Maya.';
+        }
+        return $this->render('base.html.twig', [
+            'meta' => $meta
+        ]);
     }
 
 }
